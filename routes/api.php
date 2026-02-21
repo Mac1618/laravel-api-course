ec69be13f8fe44cde31f644c2e0cb18a9ca031f7
Mac1618 <mac46099@gmail.com>
2026-02-21 21:55:11 +0800

    feat: installed sanctum, added post api rooute and PostController

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/hello', function () {
+    return ["message" => "Hello World"];
+});
+
+// posts - traditional way
+// Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+// Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+// Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+
+// posts - short cut
+Route::apiResource('posts', PostController::class);
